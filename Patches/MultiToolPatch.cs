efb3bb5a87f39a40dc43bcf7bf155b996afea960
willis81808 <willis81808@gmail.com>
2024-07-13 23:06:18 -0600

    more name changes

--- Patches/MultiToolPatch.cs
+++ Patches/MultiToolPatch.cs
@@ -1,11 +1,13 @@
-﻿namespace PortableMultiTool.Patches;
+﻿using PortableMultiTool.Items;
 
-[HarmonyPatch(typeof(Items.MultiTool))]
+namespace HackPadWeightPatch.Patches;
+
+[HarmonyPatch(typeof(MultiTool))]
 internal class MultiToolPatch
 {
     [HarmonyPrefix]
     [HarmonyPatch("Awake")]
-    static void Awake_Patch(Items.MultiTool __instance)
+    static void Awake_Patch(MultiTool __instance)
     {
         __instance.itemProperties.weight = 1;
     }
